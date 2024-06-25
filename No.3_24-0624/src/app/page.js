703cded1f82c23f3b3e1dd03d25d7f95f9961acc
Hejin0-0 <lovenstein0624@gmail.com>
2024-06-25 15:59:04 +0900

    Navigation Buttons

--- No.3_24-0624/src/app/page.js
+++ No.3_24-0624/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
+import Navigation from "@/components/Navigation";
 
 export default function Home() {
 	return (
@@ -13,6 +14,7 @@ export default function Home() {
 			/>
 			<div className="w-full h-screen">
 				{/* navigation and 3D model */}
+				<Navigation />
 				<RenderModel>
 					<Wizard />
 				</RenderModel>
