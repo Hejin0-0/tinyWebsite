4b7847d09cb7c0cb300d4ff3d6341f504bf215be
Hejin0-0 <lovenstein0624@gmail.com>
2024-06-27 15:09:30 +0900

    Github stats & Skill icons

--- No.3_24-0624/src/components/about/index.jsx
+++ No.3_24-0624/src/components/about/index.jsx
@@ -56,8 +56,8 @@ const AboutDetails = () => {
 				>
 					<img
 						className="w-full h-auto"
-						src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-						alt="CodeBucks"
+						src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=hejin0-0&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+						alt="Hejin0-0"
 						loading="lazy"
 					/>
 				</ItemLayout>
@@ -65,8 +65,8 @@ const AboutDetails = () => {
 				<ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
 					<img
 						className="w-full h-auto"
-						src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-						alt="CodeBucks"
+						src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=hejin0-0&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+						alt="Hejin0-0"
 						loading="lazy"
 					/>
 				</ItemLayout>
@@ -74,8 +74,8 @@ const AboutDetails = () => {
 				<ItemLayout className={"col-span-full"}>
 					<img
 						className="w-full h-auto"
-						src={`https://skillicons.dev/icons?i=bootstrap,c,cpp,cloudflare,css,dart,flutter,figma,git,github,html,js,jquery,linux,mysql,nextjs,nodejs,npm,react,replit,python,tailwind,threejs,vercel,vite,vscode,yarn`}
-						alt="CodeBucks"
+						src={`https://skillicons.dev/icons?i=c,cpp,cloudflare,css,dart,django,flutter,figma,git,github,html,js,jquery,linux,kotlin,mysql,nextjs,nodejs,npm,p5js,php,py,react,replit,tailwind,threejs,unreal,vercel,vscode,yarn`}
+						alt="Hejin0-0"
 						loading="lazy"
 					/>
 				</ItemLayout>
@@ -83,22 +83,22 @@ const AboutDetails = () => {
 				<ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
 					<img
 						className="w-full h-auto"
-						src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
-						alt="CodeBucks"
+						src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=hejin0-0&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+						alt="Hejin0-0"
 						loading="lazy"
 					/>
 				</ItemLayout>
 
 				<ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
 					<Link
-						href="https://github.com/Hejin0-0"
+						href="https://github.com/Hejin0-0/tinyGame"
 						target="_blank"
 						className="w-full"
 					>
 						<img
 							className="w-full h-auto"
-							src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
-							alt="CodeBucks"
+							src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=hejin0-0&repo=tinyGame&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+							alt="Hejin0-0"
 							loading="lazy"
 						/>
 					</Link>
