dbd8fcaf14d58db4a1a51f9f1a1cbeac6d9c97f2
Hejin0-0 <lovenstein0624@gmail.com>
2024-07-22 14:14:29 +0900

    About Page & CTA

---
+++ No.4_24-0710/src/constants/index.js
@@ -0,0 +1,215 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+	car,
+	contact,
+	css,
+	estate,
+	express,
+	git,
+	github,
+	html,
+	javascript,
+	linkedin,
+	mongodb,
+	motion,
+	nextjs,
+	nodejs,
+	pricewise,
+	react,
+	sass,
+	snapgram,
+	summiz,
+	tailwindcss,
+	threads,
+	typescript,
+} from "../assets/icons";
+
+export const skills = [
+	{
+		imageUrl: css,
+		name: "CSS",
+		type: "Frontend",
+	},
+	{
+		imageUrl: express,
+		name: "Express",
+		type: "Backend",
+	},
+	{
+		imageUrl: git,
+		name: "Git",
+		type: "Version Control",
+	},
+	{
+		imageUrl: github,
+		name: "GitHub",
+		type: "Version Control",
+	},
+	{
+		imageUrl: html,
+		name: "HTML",
+		type: "Frontend",
+	},
+	{
+		imageUrl: javascript,
+		name: "JavaScript",
+		type: "Frontend",
+	},
+	{
+		imageUrl: mongodb,
+		name: "MongoDB",
+		type: "Database",
+	},
+	{
+		imageUrl: motion,
+		name: "Motion",
+		type: "Animation",
+	},
+	{
+		imageUrl: nextjs,
+		name: "Next.js",
+		type: "Frontend",
+	},
+	{
+		imageUrl: nodejs,
+		name: "Node.js",
+		type: "Backend",
+	},
+	{
+		imageUrl: react,
+		name: "React",
+		type: "Frontend",
+	},
+	{
+		imageUrl: sass,
+		name: "Sass",
+		type: "Frontend",
+	},
+	{
+		imageUrl: tailwindcss,
+		name: "Tailwind CSS",
+		type: "Frontend",
+	},
+	{
+		imageUrl: typescript,
+		name: "TypeScript",
+		type: "Frontend",
+	},
+];
+
+export const experiences = [
+	{
+		title: "JavaScript Developer",
+		company_name: "Starbucks",
+		icon: starbucks,
+		iconBg: "#accbe1",
+		date: "March 2020 - April 2021",
+		points: [
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			"Mauris est eros, suscipit et gravida non, tempor non lectus.",
+			" Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis.",
+			"Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante.",
+		],
+	},
+	{
+		title: "Data Analytics",
+		company_name: "Tesla",
+		icon: tesla,
+		iconBg: "#fbc3bc",
+		date: "Jan 2021 - Feb 2022",
+		points: [
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			"Mauris est eros, suscipit et gravida non, tempor non lectus.",
+			" Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis.",
+			"Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante.",
+		],
+	},
+	{
+		title: "Web Developer",
+		company_name: "Shopify",
+		icon: shopify,
+		iconBg: "#b7e4c7",
+		date: "Jan 2022 - Jan 2023",
+		points: [
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			"Mauris est eros, suscipit et gravida non, tempor non lectus.",
+			" Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis.",
+			"Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante.",
+		],
+	},
+	{
+		title: "Full stack Developer",
+		company_name: "Meta",
+		icon: meta,
+		iconBg: "#a2d2ff",
+		date: "Jan 2023 - Present",
+		points: [
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			"Mauris est eros, suscipit et gravida non, tempor non lectus.",
+			" Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis.",
+			"Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante.",
+		],
+	},
+];
+
+export const socialLinks = [
+	{
+		name: "Contact",
+		iconUrl: contact,
+		link: "/contact",
+	},
+	{
+		name: "GitHub",
+		iconUrl: github,
+		link: "https://github.com/Hejin0-0",
+	},
+	{
+		name: "LinkedIn",
+		iconUrl: linkedin,
+		link: "https://www.linkedin.com/in/rels0w0",
+	},
+];
+
+export const projects = [
+	{
+		iconUrl: pricewise,
+		theme: "btn-back-red",
+		name: "Full Stack Clone Coding",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris est eros, suscipit et gravida non, tempor non lectus. Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis. Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante. Suspendisse eget fermentum libero. Morbi sed velit bibendum enim rutrum porta. Phasellus faucibus urna vestibulum viverra aliquet. ",
+		link: "https://github.com/Hejin0-0/tinyJavaScript",
+	},
+	{
+		iconUrl: car,
+		theme: "btn-back-blue",
+		name: "Only CSS App",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris est eros, suscipit et gravida non, tempor non lectus. Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis. Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante. Suspendisse eget fermentum libero. Morbi sed velit bibendum enim rutrum porta. Phasellus faucibus urna vestibulum viverra aliquet. ",
+		link: "https://github.com/Hejin0-0/tinyCSS",
+	},
+	{
+		iconUrl: snapgram,
+		theme: "btn-back-pink",
+		name: "Full Stack Instagram Clone",
+		description:
+			"Created clones of various websites using multiple programming languages.",
+		link: "https://github.com/Hejin0-0/tinyClonecoding",
+	},
+
+	{
+		iconUrl: estate,
+		theme: "btn-back-black",
+		name: "Real-Estate Application",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris est eros, suscipit et gravida non, tempor non lectus. Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis. Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante. Suspendisse eget fermentum libero. Morbi sed velit bibendum enim rutrum porta. Phasellus faucibus urna vestibulum viverra aliquet. ",
+		link: "https://github.com/Hejin0-0/tinyWebsite",
+	},
+	{
+		iconUrl: summiz,
+		theme: "btn-back-yellow",
+		name: "AI Summarizer Application",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris est eros, suscipit et gravida non, tempor non lectus. Donec ac nunc a urna faucibus auctor. Ut suscipit sit amet orci sed venenatis. Vivamus sit amet quam hendrerit, aliquet justo sed, pretium ante. Suspendisse eget fermentum libero. Morbi sed velit bibendum enim rutrum porta. Phasellus faucibus urna vestibulum viverra aliquet. ",
+		link: "https://github.com/Hejin0-0/tinyGame",
+	},
+];
