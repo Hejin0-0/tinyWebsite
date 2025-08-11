9a8f3bbadaeb61e0a8b329410d6cac0e34b9a766
Hejin0-0 <lovenstein0624@email.com>
2025-08-11 16:12:23 +0900

    feat: Configure tRPC

--- no.5_25-0811/src/app/page.tsx
+++ no.5_25-0811/src/app/page.tsx
@@ -1,9 +1,19 @@
-const Page = () => { 
+import { Suspense } from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+
+const Page = async () => { 
+  const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "MwaMa"}))
 
   return (
-    <div>
-      Hello
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+
+      <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
