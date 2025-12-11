387d870868342dce0d6f2218e38c8e4f7fe01c64
Hejin0-0 <lovenstein0624@email.com>
2025-12-11 14:26:14 +0900

    Feat: Add File path Breadcrumbs and navigation tabs to Project View

--- no.5_25-0811/src/modules/projects/ui/views/project-view.tsx
+++ no.5_25-0811/src/modules/projects/ui/views/project-view.tsx
@@ -8,15 +8,21 @@ import {
 import { MessagesContainer } from "../components/messages-container";
 import { Suspense, useState } from "react";
 import { Fragment } from "@/generated/prisma";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ProjectHeader } from "../components/project-header";
 import { FragmentWeb } from "../components/fragment-web";
+import { EyeIcon, CodeIcon, CrownIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FileExplorer } from "@/components/file-explorer";
 
 interface Props {
     projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
-    const [activeFragment, setActiveFragment] = useState<Fragment | null >(null)
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null)
+    const [tabSate, setTabState] = useState<"preview" | "code">("preview")
 
     return (
         <div className="h-screen">
@@ -30,16 +36,49 @@ export const ProjectView = ({ projectId }: Props) => {
                         <ProjectHeader projectId={projectId} />
                     </Suspense>
                     <Suspense fallback={<p>Loading messages...</p>}>
-                        <MessagesContainer 
+                        <MessagesContainer
                             projectId={projectId}
                             activeFragment={activeFragment}
                             setActiveFragment={setActiveFragment}
-                            />
+                        />
                     </Suspense>
                 </ResizablePanel>
-                <ResizableHandle withHandle/>
+                <ResizableHandle withHandle />
                 <ResizablePanel defaultSize={65} minSize={50}>
-                    {!!activeFragment && <FragmentWeb data={activeFragment} />}
+                    <Tabs
+                        className="h-full gap-y-0"
+                        defaultValue="preview"
+                        value={tabSate}
+                        onValueChange={(value) => setTabState(value as "preview" | "code")}
+                    >
+                        <div className="w-full flex items-center p-2 border-b gap-x-2">
+                            <TabsList className="h-8 p-0 border rounded-md" >
+                                <TabsTrigger value="preview" className="rounded-md" >
+                                    <EyeIcon /> <span>Demo</span>
+                                </TabsTrigger>
+                                <TabsTrigger value="code" className="rounded-md" >
+                                    <CodeIcon /> <span>Code</span>
+                                </TabsTrigger>
+                            </TabsList>
+                            <div className="ml-auto flex items-center gap-x-2">
+                                <Button asChild size='sm' variant='default'>
+                                    <Link href="/pricing" >
+                                        <CrownIcon /> Upgrade
+                                    </Link>
+                                </Button>
+                            </div>
+                        </div>
+                        <TabsContent value="preview">
+                            {!!activeFragment && <FragmentWeb data={activeFragment} />}
+                        </TabsContent>
+                        <TabsContent value="code" className="min-h-0" >
+                            {!!activeFragment?.files && (
+                                <FileExplorer
+                                    files={activeFragment?.files as { [path: string]: string }}
+                                />
+                            )}
+                        </TabsContent>
+                    </Tabs>
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
