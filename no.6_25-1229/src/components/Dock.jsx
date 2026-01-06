36b766d82cb07b1466a6fb83cbe7f0966d5c6015
Hejin0-0 <lovenstein0624@email.com>
2026-01-06 17:03:41 +0900

    Feat: Build a robust window management system for multi-tasking UI

--- no.6_25-1229/src/components/Dock.jsx
+++ no.6_25-1229/src/components/Dock.jsx
@@ -4,9 +4,11 @@ import gsap from "gsap"
 
 import { dockApps } from "#constants/index.js"
 import { useGSAP } from "@gsap/react"
+import useWindowStore from "#store/window.js"
 
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows } = useWindowStore()
     const dockRef = useRef(null)
 
     useGSAP(() => {
@@ -54,7 +56,20 @@ const Dock = () => {
 
 
     const toggleApp = (app) => {
-        // TODO : Implement Open Window Logic
+        if (!app.canOpen) return;
+
+        const window = windows[app.id]
+
+        if (!window) {
+            console.error(`Window not found for app: ${app.id}`)
+            return;
+        }
+
+        if (window.isOpen) {
+            closeWindow(app.id)
+        } else {
+            openWindow(app.id)
+        }
     }
 
 
